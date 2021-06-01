b475f3e02fd01c23535f23309712d50059919273
Vladimir Hambardzumyan <vladimirhambardzumyan24@gmail.com>
2021-06-01 11:58:51 +0400

    concat arrays.

--- concat.js
+++ concat.js
@@ -1,12 +1,13 @@
-let newArr = [];
-function concats(arr) {
+
+function concats(arr, newArr) {
   arr.map(function (val) {
     if (Array.isArray(val)) {
-      concats(val);
+      concats(val,newArr);
     } else {
       newArr.push(val);
     }
   });
   return newArr;
 }
-console.log(concats([14, [1, [[[3, []]], 1], 0]], 0));
+console.log(concats([14, [1, [[[3, []]], 1], 0]], []));
+console.log(concats([14, [1, [[[3, []]], 1], 0]], []));
